37f897b9cda3cc011341336524744740ce895faa
karbja <62626773+karbja@users.noreply.github.com>
2023-05-03 12:47:03 +0200

    Delete Voter directory

--- Voter/global.js
+++
@@ -1,8 +0,0 @@
-import { Platform, LogBox } from "react-native";
-if (Platform.OS !== "web") {
-    require("react-native-get-random-values");
-    LogBox.ignoreLogs([
-      "Warning: The provided value 'ms-stream' is not a valid 'responseType'.",
-      "Warning: The provided value 'moz-chunked-arraybuffer' is not a valid 'responseType'.",
-    ]);
-  }
